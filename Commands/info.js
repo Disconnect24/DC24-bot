6a9965d4b1ea679a0a348a7125ce45179ab27a28
WiiNoMa <45322188+WiiNoMa@users.noreply.github.com>
2018-12-02 16:00:12 -0800

    Update info.js

--- Commands/info.js
+++ Commands/info.js
@@ -3,13 +3,13 @@ module.exports = {
     run: function(bot, config, msg, args, suffix, Discord, color) {
 
         let embed = new Discord.RichEmbed()
-            .setTitle(`Disconnect24 Bot - Information`)
+            .setTitle(`DC24 Bot - Information`)
             .setDescription(`https://github.com/Disconnect24/DC24-bot`)
             .addField(`Developed by:`, `TheMegaEagle`)
             .addField(`Contributors:`, `Wiiの間 (Error Suggestions), CornierKhan1 (VPS), Apfel (Important Suggestions)`)
             .addField(`Running since:`, `${bot.readyAt}`)
             .addField(`Bot Invite:`, `${config.botInvite}`)
-            .addField(`Disconnect24 Invite:`, `${config.serverInvite}`)
+            .addField(`DC24 Invite:`, `${config.serverInvite}`)
             .setFooter(`Become a Contributor by submitting an important pull request to our github page! (https://github.com/Disconnect24/DC24-bot)`)
             .setColor(color)
 
