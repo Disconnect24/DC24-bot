50daf41bb1d4df29951ae02b9aa95db2e6b5e112
Michael <34601590+TheMegaEagle@users.noreply.github.com>
2018-12-12 18:45:37 -0500

    Update user.js
    soon:tm:

--- Commands/user.js
+++ Commands/user.js
@@ -1,55 +1,33 @@
 module.exports = {
 
-    run: function(bot, config, msg, args, suffix, Discord, blError, gbError, dmError, owError, sfError, prError, otError, chError, mbError, names, sleep) {
+    run: function(bot, config, msg, args, suffix, Discord, color) {
 
 
         if (msg.channel.type === "dm") {
-            msg.react(`❌`)
-            msg.channel.send(dmError).then(function(m) {
-                sleep(2000)
-                m.delete()
-              })
+            msg.channel.send(`This command must be run in a server.`)
             return;
         }
-
-        if (msg.channel.type === "text") {
-            msg.react(`✅`)
-        } 
-
+        
         const member = msg.mentions.members.first();
 
-        if (!suffix) {
+        if (!member) {
             let ownUser = new Discord.RichEmbed()
                 .setAuthor(msg.member.user.tag, msg.member.user.avatarURL, null)
-                .addField(`Username:`, msg.member.user.username)
-                .addField(`Nickname:`, msg.member.nickname)
-                .addField(`Tag:`, msg.member.user.discriminator)
                 .addField(`ID:`, msg.member.user.id)
                 .addField(`Account Created:`, msg.member.user.createdAt)
                 .addField(`Joined Server:`, msg.member.joinedAt)
-                //.addField(`Game:`, msg.member.user.presence.game.name)
-                .addField(`Requested In:`, msg.guild.name)
-                .setColor(msg.member.highestRole.hexColor)
-                .setThumbnail(bot.user.avatarURL)
-                .setFooter(names)
+                .setColor(color)
             msg.channel.send(ownUser)
             return;
         }
 
-        if (suffix) {
+        if (member) {
             let otherUser = new Discord.RichEmbed()
                 .setAuthor(member.user.tag, member.user.avatarURL, null)
-                .addField(`Username:`, member.user.username)
-                .addField(`Nickname:`, member.nickname)
-                .addField(`Tag:`, member.user.discriminator)
                 .addField(`ID:`, member.user.id)
                 .addField(`Account Created:`, member.user.createdAt)
                 .addField(`Joined Server:`, member.joinedAt)
-                //.addField(`Game:`, member.user.presence.game.name)
-                .addField(`Requested In:`, msg.guild.name)
                 .setColor(member.highestRole.hexColor)
-                .setThumbnail(bot.user.avatarURL)
-                .setFooter(names)
             msg.channel.send(otherUser)
             return;
         }
