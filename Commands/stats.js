480a94658c8bd52cce7541ddd695054cdcc3837d
Michael <34601590+TheMegaEagle@users.noreply.github.com>
2018-12-15 05:30:59 -0500

    Update stats.js

--- Commands/stats.js
+++ Commands/stats.js
@@ -1,11 +1,19 @@
+const fs = require("fs");
+const stats = JSON.parse(fs.readFileSync("./Settings/stats.json", "utf8"))
+
 module.exports = {
 
     run: function(bot, config, msg, args, suffix, Discord, color) {
 
-        
+        let embed = new Discord.RichEmbed()
+            .setTitle(`Disconnect24 Service Stats`)
+            .addField(`Mail`, stats.mail)
+            .addField(`Check Mii Out Channel`, stats.cmoc)
+            .setColor(color)
+
+        msg.channel.send(embed)
 
     }
-  
   
   }
   
