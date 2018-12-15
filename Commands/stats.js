9fc311da247265a43a0fcdc26b1f3bdfa4884557
Marko <32610623+Apfel@users.noreply.github.com>
2018-12-15 14:16:55 +0100

    Fixed variables

--- Commands/stats.js
+++ Commands/stats.js
@@ -7,16 +7,16 @@ module.exports = {
 
         let embed = new Discord.RichEmbed()
             .setTitle(`Disconnect24 Service Stats`)
-            .setDescription(`Check Mii Out Channel: **{stats.cmoc}**\n
-            Disconnect24 Channel: **{stats.dc24channel}**\n
-            Everybody Votes Channel: **{stats.evc}**\n
-            Forecast Channel: **{stats.fore}**\n
-            Japanese Channels: **{stats.japan}**\n
-            Mail: **{stats.mail}**\n
-            Miiverse: **{stats.miiverse}**\n
-            News Channel: **{stats.news}**\n
-            Nintendo Channel: **{stats.nc}**\n
-            Wii Speak Channel: **{stats.speak}**`)
+            .setDescription(`Check Mii Out Channel: **${stats.cmoc}**\n
+            Disconnect24 Channel: **${stats.dc24channel}**\n
+            Everybody Votes Channel: **${stats.evc}**\n
+            Forecast Channel: **${stats.fore}**\n
+            Japanese Channels: **${stats.japan}**\n
+            Mail: **${stats.mail}**\n
+            Miiverse: **${stats.miiverse}**\n
+            News Channel: **${stats.news}**\n
+            Nintendo Channel: **${stats.nc}**\n
+            Wii Speak Channel: **${stats.speak}**`)
             .setColor(color)
 
         msg.channel.send(embed)
