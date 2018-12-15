e4e8c82bab36d0c0281f1684bb9d7b8ad8256187
Apfel <32610623+Apfel@users.noreply.github.com>
2018-12-15 18:18:12 +0100

    Fixed embed once again

--- Commands/info.js
+++ Commands/info.js
@@ -4,19 +4,14 @@ module.exports = {
 
         let embed = new Discord.RichEmbed()
             .setTitle(`Disconnect24 Bot - Information`)
-            .setDescription(`https://github.com/Disconnect24/DC24-bot`)
-            .addField(`Developed by:`, `TheMegaEagle`)
-            .addField(`Contributors:`, `Wiiの間 (Error Suggestions), CornierKhan1 (VPS), Apfel (Important Suggestions)`)
-            .addField(`Running since:`, `${bot.readyAt}`)
-            .addField(`Bot Invite:`, `${config.botInvite}`)
-            .addField(`Disconnect24 Invite:`, `${config.serverInvite}`)
-            .setFooter(`Become a Contributor by submitting an important pull request to our github page! (https://github.com/Disconnect24/DC24-bot)`)
+            .setDescription(`**[GitHub page](https://github.com/Disconnect24/DC24-bot)**
+            Developed by: **<@${config.creatorID}>**\n
+            Contributors: **Wiiの間** (Error Suggestions), **CornierKhan1** (VPS), **Apfel** (Bot improvements)
+            Running since: **${bot.readyAt}**`)
+            .addField(`Links`, `**[Bot Invite](${config.botInvite})**\n**[Disconnect24 Invite](${config.serverInvite})**`)
+            .setFooter(`Become a Contributor by submitting an important pull request to our GitHub page!`)
             .setColor(color)
 
         msg.channel.send(embed)
-        msg.react(`✅`)
-
     }
-  
-  }
-  
+}
