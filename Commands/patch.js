afc7f1643b9c92de01937aa7b3ed118546035782
Michael <34601590+TheMegaEagle@users.noreply.github.com>
2018-12-16 15:14:43 -0500

    Update patch.js
    not done yet

--- Commands/patch.js
+++ Commands/patch.js
@@ -1,8 +1,20 @@
+const axios = require('axios')
+
 module.exports = {
 
     run: function(bot, config, msg, args, suffix, Discord, color) {
-
-
+        
+        axios.post('https://mail.service.dc24.xyz/patch', {
+          uploaded_config: 'msg.attachments.first().url'
+        })
+        .then((res) => {
+          console.log(`statusCode: ${res.statusCode}`)
+          console.log(res)
+        })
+        .catch((error) => {
+          msg.channel.send(error)
+        })
+        
     }
   
   }
