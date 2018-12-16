341a334f09f0d35acd6ddc2b1a71e573fcdfb4dc
Michael <34601590+TheMegaEagle@users.noreply.github.com>
2018-12-16 12:07:24 -0500

    Update server.js
    Removing icon command, simplifying it for icon to be in server command

--- Commands/server.js
+++ Commands/server.js
@@ -7,17 +7,35 @@ module.exports = {
             msg.channel.send(`This command must be run in a server.`)
             return;
         }
-
-        let iconEmbed = new Discord.RichEmbed()
-            .setAuthor(msg.guild.name, msg.guild.iconURL, null)
-            .addField(`ID:`, msg.guild.id)
-            .addField(`Server Created:`, msg.guild.createdAt)
-            .addField(`Region:`, msg.guild.region)
-            .addField(`Verification Level:`, msg.guild.verificationLevel)
-            .addField(`Owner`, msg.guild.owner)
-            .addField(`Members:`, msg.guild.memberCount)
-            .setColor(color)
-        msg.channel.send(iconEmbed)
+        
+        if (msg.guild.iconURL) {
+            let iconEmbed = new Discord.RichEmbed()
+                .setAuthor(msg.guild.name, msg.guild.iconURL, null)
+                .addField(`ID:`, msg.guild.id)
+                .addField(`Server Created:`, msg.guild.createdAt)
+                .addField(`Region:`, msg.guild.region)
+                .addField(`Verification Level:`, msg.guild.verificationLevel)
+                .addField(`Owner`, msg.guild.owner)
+                .addField(`Members:`, msg.guild.memberCount)
+                .setThumbnail(msg.guild.iconURL)
+                .setColor(color)
+            msg.channel.send(iconEmbed)
+            return;
+        }
+        
+        if (!msg.guild.iconURL) {
+            let iconEmbed = new Discord.RichEmbed()
+                .setAuthor(msg.guild.name, msg.guild.iconURL, null)
+                .addField(`ID:`, msg.guild.id)
+                .addField(`Server Created:`, msg.guild.createdAt)
+                .addField(`Region:`, msg.guild.region)
+                .addField(`Verification Level:`, msg.guild.verificationLevel)
+                .addField(`Owner`, msg.guild.owner)
+                .addField(`Members:`, msg.guild.memberCount)
+                .setColor(color)
+            msg.channel.send(iconEmbed)
+            return;
+        }
         
     }
   
