39bcbdb5eeba47cac06150a82feab1ac5f5299e0
Michael <34601590+TheMegaEagle@users.noreply.github.com>
2018-12-16 20:20:47 -0500

    Update patch.js
    This code works but it needs to be finished

--- Commands/patch.js
+++ Commands/patch.js
@@ -1,19 +1,24 @@
 const axios = require('axios')
+const download = require('download-file')
 
 module.exports = {
 
     run: function(bot, config, msg, args, suffix, Discord, color) {
         
-        axios.post('https://mail.service.dc24.xyz/patch', {
-          uploaded_config: 'msg.attachments.first().url'
+        var url = "https://github.com/RiiConnect24-Bot/RC24-Bot/raw/5ef38c7146a9fe6a0edc977399de20cc64c10acc/src/test/resources/correct_orig.cfg"
+        let userConfig = request.get(url);
+        request.put('https://mail.service.dc24.xyz/patch', {
+            formData: {
+               uploaded_config: userConfig
+            }
         })
-        .then((res) => {
-          console.log(`statusCode: ${res.statusCode}`)
-          console.log(res)
+        .on('response', (response) => {
+            msg.channel.send(response.statusCode);
+            console.log('response', response);
         })
-        .catch((error) => {
-          msg.channel.send(error)
-        })
+        .on('error', (err) => {
+            console.log(err);
+         });
         
     }
   
