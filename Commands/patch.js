6a5aced17eb3250b51cd01a20c6d7c81c03cd347
Michael <34601590+TheMegaEagle@users.noreply.github.com>
2018-12-22 17:09:28 -0500

    Patch update - this **should** work!

--- Commands/patch.js
+++ Commands/patch.js
@@ -4,25 +4,25 @@ module.exports = {
 
     run: function(bot, config, msg, args, suffix, Discord, color) {
         
-        var url = msg.attachments.first().url
+        var sentfile = msg.attachments.first()
+        if (!sentfile) return msg.channel.send(`You must attach a file.`)
+        var url = sentfile.url
+        
         let userConfig = request.get(url);
-        request.put('https://mail.service.dc24.xyz/patch', {
-            formData: {
+
+        formData: {
                uploaded_config: userConfig
             }
-        })
-        .on('response', (response) => {
-            msg.channel.send(response.statusCode);
-            console.log('response', response);
-        })
-        .on('error', (err) => {
-            console.log(err);
-         });
-        
 
-        const buffer = fs.readFileSync('./Configs/nwc24msg.cfg');
-        const attachment = new Attachment(buffer, 'nwc24msg.cfg');
-        msg.author.send(`Mail patching complete.`, attachment);
+        request.post({url:'https://mail.service.dc24.xyz/patch', formData: {uploaded_config: userConfig}}, function optionalCallback(err, httpResponse, body) {
+              if (err) {
+                return console.error('upload failed:', err);
+              }
+              console.log('Upload successful!  Server responded with:', body);
+              const buffer = request.body
+              const attachment = new Attachment(buffer, 'nwc24msg.cfg');
+              msg.author.send(`Mail patching complete.`, attachment);
+        });
 
         
     }
