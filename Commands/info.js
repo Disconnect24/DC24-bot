3a1b92fff4490e81af2436f8cd111b11fd3f22f8
Spotlight <spotlight@joscomputing.space>
2018-12-22 22:59:35 -0600

    Use discord.js' invite creation function over static

--- Commands/info.js
+++ Commands/info.js
@@ -1,17 +1,23 @@
 module.exports = {
 
-    run: function(bot, config, msg, args, suffix, Discord, color) {
-
-        let embed = new Discord.RichEmbed()
-            .setTitle(`Disconnect24 Bot - Information`)
-            .setDescription(`**[GitHub page](https://github.com/Disconnect24/DC24-bot)**
-            Developed by: **<@${config.creatorID}>**\n
-            Contributors: **Wiiの間** (Error Suggestions), **CornierKhan1** (VPS), **Apfel** (Bot improvements)
-            Running since: **${bot.readyAt}**`)
-            .addField(`Links`, `**[Bot Invite](${config.botInvite})**\n**[Disconnect24 Invite](${config.serverInvite})**`)
-            .setFooter(`Become a Contributor by submitting an important pull request to our GitHub page!`)
-            .setColor(color)
+  run: function(bot, config, msg, args, suffix, Discord, color) {
+    bot.generateInvite(['VIEW_CHANNEL', 'SEND_MESSAGES', 'MANAGE_MESSAGES',
+                        'EMBED_LINKS', 'ATTACH_FILES', 'READ_MESSAGE_HISTORY',
+                        'USE_EXTERNAL_EMOJIS', 'ADD_REACTIONS'])
+    .then(function(link){
+      let embed = new Discord.RichEmbed()
+        .setTitle(`Disconnect24 Bot - Information`)
+        .setDescription(`**[GitHub page](https://github.com/Disconnect24/DC24-bot)**
+              Developed by: **<@${config.creatorID}>**\n
+              Contributors: **Wiiの間** (Error Suggestions), **CornierKhan1** (VPS), **Apfel** (Bot improvements)
+              Running since: **${bot.readyAt}**`)
+        .addField(`Links`, `**[Bot Invite](${link})**\n**[Disconnect24 Invite](${config.serverInvite})**`)
+        .setFooter(`Become a Contributor by submitting an important pull request to our GitHub page!`)
+        .setColor(color)
 
         msg.channel.send(embed)
-    }
+    }, function(error) {
+      console.error;
+    });
+  }
 }
