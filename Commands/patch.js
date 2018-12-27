b4c81a537e7105238309330df880d60ef222485e
Michael <34601590+TheMegaEagle@users.noreply.github.com>
2018-12-27 06:59:25 -0500

    Update patch.js

--- Commands/patch.js
+++ Commands/patch.js
@@ -16,8 +16,7 @@ module.exports = {
         
         let userConfig = request.get(url);
 
-        request.post({url:'https://mail.service.dc24.xyz/patch', headers: { 'Content-Type': 'multipart/form-data', 'Boundary': '----WebKitFormBoundarytUArugCRlbmMdP4C
-' }, formData: {uploaded_config: userConfig}}, function optionalCallback(err, httpResponse, body) {
+        request.post({url:'https://mail.service.dc24.xyz/patch', headers: { 'Content-Type': 'multipart/form-data', 'boundary': 'dc24bot' }, formData: {uploaded_config: userConfig}}, function optionalCallback(err, httpResponse, body) {
               if (err) {
                 return msg.channel.send(`Upload failed: ${err}`);
               }
