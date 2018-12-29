4bda12c21be0ae92e6f833d86e35c2cfd1086684
Michael <34601590+TheMegaEagle@users.noreply.github.com>
2018-12-29 09:47:42 -0500

    Update evc-suggest.js

--- Commands/evc-suggest.js
+++ Commands/evc-suggest.js
@@ -1,7 +1,17 @@
+const fs = require("fs");
+const details = JSON.parse(fs.readFileSync("./Settings/webhook.json", "utf8"))
+
 module.exports = {
 
     run: function(bot, config, msg, args, suffix, Discord, color) {
+        if (!suffix) {
+            msg.channel.send(`You must have something written for your suggestion.`)
+        }
+        
+        const webhook1 = new Discord.WebhookClient(details.id, details.token)
+        webhook1.send(`${msg.author.tag} (User ID: ${msg.author.id}) has suggested "${suffix}" as a new Everybody Votes Channel question!`)
 
+        msg.channel.send(`Your suggestion has been sent to the developers!`)
     }
   
   }
