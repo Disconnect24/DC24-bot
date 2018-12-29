eee167d43fa7fbe650790a86cdd7eac7abcc4f67
Michael <34601590+TheMegaEagle@users.noreply.github.com>
2018-12-29 09:26:32 -0500

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -12,6 +12,7 @@ module.exports = {
             .addField(`${config.prefix}patchers`, `Sends patchers that can be used to connect to Disconnect24.`)
             .addField(`${config.prefix}info`, `Sends information related to the bot.`)
             .addField(`${config.prefix}set-code`, `Allows you to add your Disconnect24 Wii Mail code to the database.`)
+            .addField(`${config.prefix}remove-code`, `Allows you to remove your Disconnect24 Wii Mail code from the database.`)
             .addField(`${config.prefix}stats`, `Gets the stats of DC24 services.`)
             .addField(`${config.prefix}patch`, `Allows you to patch your nwc24msg.cfg for DC24 use.`)
             .addField(`${config.prefix}code`, `Allows you to fetch a Disconnect24 Wii Mail code of either yourself or a mentioned user.`)
