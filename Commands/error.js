02e4ef58f3963596ae91016d3e00fbef27455bc1
Michael <34601590+TheMegaEagle@users.noreply.github.com>
2018-12-30 13:46:53 -0500

    okay, this is epic (wiimmfi api)

--- Commands/error.js
+++ Commands/error.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const errors = JSON.parse(fs.readFileSync("./Settings/error-db.json", "utf8"))
 
+const request = require('request')
+
 module.exports = {
 
     run: function(bot, config, msg, args, suffix, Discord, color) {
@@ -11,8 +13,25 @@ module.exports = {
         }
 
         if (!errors[suffix]) {
-            msg.channel.send(`That error was not found in the Disconnect24 database! However, feel free to add it using a PR or by DMing a developer!`)
-            return;
+            //msg.channel.send(`That error was not found in the Disconnect24 database! However, feel free to add it using a PR or by DMing a developer!`)
+            //return;
+            let link = "https://wiimmfi.de/error?e=" + suffix + "&m=json"
+            
+            request.get({url: link}, function(err, response) {
+              if (err) {
+                return msg.channel.send(`Unknown Error: ${err}`);
+              }
+              const wiimmfierror = JSON.parse(response)
+
+              let embed1 = new Discord.RichEmbed()
+                  .setTitle(`Here is some info on error ${suffix}.`)
+                  .setDescription(wiimmfierror[infolist].error)
+                  .setColor(color)
+                  .setFooter(`This error was found using the Wiimmfi API.`)
+
+              msg.channel.send(embed1)
+
+            });
         }
 
         let embed = new Discord.RichEmbed()
